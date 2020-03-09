1f610a038685b70bd8d21f2dc8a9c3d29722417e
catcarbonell <hello@catcarbn.com>
2020-03-09 09:33:00 -0700

    lol I kept going.

--- js/popop.js
+++ js/popop.js
@@ -1,1 +1,25 @@
-console.log("rororororor");
+/* POPOP.JS - FUNCTIONALITY FOR YOUR MODAL */
+const modalBtn = document.querySelector('.popop-btn')
+const modal = document.querySelector('.popop-modal');
+const modalBgLite = document.querySelector('.popop-bg-lite')
+const closeBtn = document.querySelector('.close-btn');
+
+function popop(el){
+    modalBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        el.classList.add('popop-bg-lite');
+        modalBgLite.style.display = 'initial';
+        el.style.display = 'flex';
+    });
+};
+
+function closeModal(el){
+    closeBtn.addEventListener('click', function (e) {
+        el.style.display = 'none';
+        modalBgLite.style.display = 'none';
+        el.classList.remove('popop-bg-lite');
+    });
+};
+
+popop(modal);
+closeModal(modal);
